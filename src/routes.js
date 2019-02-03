a53cae730a09a4d53d8b328840423ae4a260f9ec
DanielCarlos <daniel.carlos.souza@gmail.com>
2019-02-03 12:04:17 -0200

    ajuste rota...

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,10 @@ const routes  = express.Router();
 
 const ProductController  = require('./controllers/ProductController');
 
-routes.get("/", (res,req) => {
-    res.send("It's Run..");
+routes.get('/',(req, res)=> {
+    res.send("It's Run");
+  });
 
-});
 routes.get("/products", ProductController.index);
 routes.get("/products/:id", ProductController.show);
 
