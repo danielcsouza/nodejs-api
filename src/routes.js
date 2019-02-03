6b1015edec72b3576fd20dc723ad482500866c3f
DanielCarlos <daniel.carlos.souza@gmail.com>
2019-02-03 11:52:49 -0200

    rota

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,12 @@ const routes  = express.Router();
 
 const ProductController  = require('./controllers/ProductController');
 
+routes.get("/", (res,req) => {
+
+    return "It's Run..";
+
+
+});
 routes.get("/products", ProductController.index);
 routes.get("/products/:id", ProductController.show);
 
